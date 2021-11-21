2fa4f577cc218fbc14749865f30b8a38b244d913
Mayank Agarwal <mayank265@gmail.com>
2021-11-22 01:10:06 +0530

    End Sem Assignment Question

---
+++ tut07/tut07.py
@@ -0,0 +1,10 @@
+def feedback_not_submitted():
+
+	
+	ltp_mapping_feedback_type = {1: 'lecture', 2: 'tutorial', 3:'practical'}
+	output_file_name = "course_feedback_remaining.xlsx" 
+ 
+
+
+
+feedback_not_submitted()
